f2331894ef97de3d3654c9f9970c7ffc259c797a
GMDorneles <gabrielly.maldonadoo@gmail.com>
2021-07-31 16:10:19 -0400

    commit teste

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';//oiii
+import React, {useState, useEffect} from 'react';//ola
 import {Text, View,Button } from 'react-native';
 import {css} from './assets/CSS/css';
 import { NavigationContainer } from '@react-navigation/native';
