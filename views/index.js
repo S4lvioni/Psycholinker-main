5ec83f6178650314b016acafc1038d8140fc935d
s4lvioni <you@example.com>
2021-08-24 16:36:08 -0400

    mini-fix

--- views/index.js
+++ views/index.js
@@ -1,4 +1,4 @@
 import Home from './Home';
 import Login from './Login';
-import HomePaciente from './HomePaciente';
+import HomePaciente from './HomePaciente/HomePaciente';
 export{Home, Login,HomePaciente};
