8ffd87d37cd6c76d316a87bdb212c4cffa1a9e9f
S4lvioni <alexandretsalvioni@gmail.com>
2021-09-11 17:33:25 -0400

    Adicionado nome paciente lista

--- views/HomeTerapeuta/HomeTerapeuta.js
+++ views/HomeTerapeuta/HomeTerapeuta.js
@@ -1,5 +1,5 @@
-import React, {useState,useEffect, isValidElement} from 'react';
-import {Text, View, Modal,TouchableOpacity,TextInput,  Pressable} from 'react-native';
+import React, { useState, useEffect, isValidElement } from 'react';
+import { Text, View, Modal, TouchableOpacity, TextInput, Pressable } from 'react-native';
 //import AsyncStorage from '@react-native-community/async-storage';
 import { AsyncStorage } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
@@ -9,37 +9,38 @@ import config from '../../config/config.json';
 
 export default function HomeTerapeuta() {
     //variaveis de controle
-    const [execucao,setExecucao]=useState(1);
+    const [execucao, setExecucao] = useState(1);
     const [modalVisible, setModalVisible] = useState(false);
     //terapeuta
-    const [email,setEmail]=useState(null);
-    const [name,setName]=useState(null);
+    const [email, setEmail] = useState(null);
+    const [name, setName] = useState(null);
     //criar paciente
-    const [code,setCode]=useState(null);
-    const [codeA,setCodeA]=useState(null);
-    const [terapeutaId,setTerapeuta]=useState(null);
+    const [code, setCode] = useState(null);
+    const [codeA, setCodeA] = useState(null);
+    const [terapeutaId, setTerapeuta] = useState(null);
     //listagem
-    const [pacientes,setPacientes]=useState([
+    const [pacientes, setPacientes] = useState([
         {
-            name: null, id : '1'
+            name: null, id: '1'
         }
     ]);
     //edit paciente
-    const [pacienteName,setpacienteName]=useState(null);
-    const [pacienteTelefone,setpacienteTelefone]=useState(null);
-    const [pacienteEmail,setpacienteEmail]=useState(null);
+    const [pacienteName, setpacienteName] = useState(null);
+    const [pacienteTelefone, setpacienteTelefone] = useState(null);
+    const [pacienteEmail, setpacienteEmail] = useState(null);
     const [response, setResponse] = useState(null);
-    useEffect(()=>{
+    const [paName, setpaName] = useState(null)
+    useEffect(() => {
         randomCode();
-    },[]);
+    }, []);
 
-    useEffect(()=>{
+    useEffect(() => {
         getTerapeuta();
-    },[]);
+    }, []);
 
-    useEffect(()=>{
+    useEffect(() => {
         gerenciaPaciente();
-    },[execucao]);
+    }, [execucao]);
 
     async function gerenciaPaciente() {
         let response = await fetch(`${config.urlRoot}listaPaciente`, {
@@ -51,7 +52,7 @@ export default function HomeTerapeuta() {
             body: JSON.stringify({
                 terapeutaId: terapeutaId
             })
-            
+
         });
         //obtem resposta do controller
         let json = await response.json();
@@ -59,46 +60,48 @@ export default function HomeTerapeuta() {
             console.log('error');
         } else {
             // //persistencia dos dados para utilizar na aplicação
-           await AsyncStorage.setItem('pacientesData', JSON.stringify(json));//json é  a resposta
-          
-            let response=await AsyncStorage.getItem('pacientesData');
+            await AsyncStorage.setItem('pacientesData', JSON.stringify(json));//json é  a resposta
+
+            let response = await AsyncStorage.getItem('pacientesData');
             const jsonNovo = JSON.parse(response);
             setPacientes(jsonNovo);
-            if(execucao<2){
+            if (execucao < 2) {
                 setExecucao(2);
             }
-           
+
         }
     }
-    async function onEdit(id){
-      /*  let response = await fetch(config.urlRoot + 'editPaciente', {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                id:id
-            })
-        });
-        //obtem resposta do controller
-        let json = await response.json();
-        if (json === 'error') {
-            console.log('error');
-        } else {
-            // //persistencia dos dados para utilizar na aplicação
-           await AsyncStorage.setItem('pacientesEditData', JSON.stringify(json));//json é  a resposta
-          
-            let response=await AsyncStorage.getItem('pacientesEditData');
-            const jsonNovo = JSON.parse(response);
-        }*/
-        console.log(pacienteName);
-       /// setModalVisible(true);
-        
+    async function onEdit(id, nome) {
+        /*  let response = await fetch(config.urlRoot + 'editPaciente', {
+              method: 'POST',
+              headers: {
+                  Accept: 'application/json',
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({
+                  id:id
+              })
+          });
+          //obtem resposta do controller
+          let json = await response.json();
+          if (json === 'error') {
+              console.log('error');
+          } else {
+              // //persistencia dos dados para utilizar na aplicação
+             await AsyncStorage.setItem('pacientesEditData', JSON.stringify(json));//json é  a resposta
+            
+              let response=await AsyncStorage.getItem('pacientesEditData');
+              const jsonNovo = JSON.parse(response);
+          }*/
+        console.log(nome);
+        setModalVisible(true);
+        console.log(id);
+        setpaName(nome);
+
 
     }
 
-    async function onDelete(id){
+    async function onDelete(id) {
         let response = await fetch(config.urlRoot + 'deletePaciente', {
             method: 'POST',
             headers: {
@@ -106,78 +109,74 @@ export default function HomeTerapeuta() {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                id:id
+                id: id
             })
         })
     }
-    
+
     //pega nome para o bem vindo o
-    useEffect(()=>{
-        async function getName()
-        {
-            let response=await AsyncStorage.getItem('emailData');
-            let json=JSON.parse(response);
+    useEffect(() => {
+        async function getName() {
+            let response = await AsyncStorage.getItem('emailData');
+            let json = JSON.parse(response);
             setName(json.name);
             setEmail(json.email);
         }
         getName();
-    },[]);
+    }, []);
 
-       //Pegar o id do Terapeuta
-       async function getTerapeuta()
-       {
-           let response=await AsyncStorage.getItem('emailData');
-           let json=JSON.parse(response);
-           setTerapeuta(json.id);
-       }
+    //Pegar o id do Terapeuta
+    async function getTerapeuta() {
+        let response = await AsyncStorage.getItem('emailData');
+        let json = JSON.parse(response);
+        setTerapeuta(json.id);
+    }
 
-        //Gerar um código randômico
-        //Em caso de uso real fazer alterações de segurança**
-        async function randomCode()
-        {
-            let result = '';
-            let length=5;
-            let chars='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-            for (let i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
-            console.log(result);
-            setCode(result);
+    //Gerar um código randômico
+    //Em caso de uso real fazer alterações de segurança**
+    async function randomCode() {
+        let result = '';
+        let length = 5;
+        let chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        for (let i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+        console.log(result);
+        setCode(result);
+    }
+    //Envio do formulário
+    async function sendForm() {
+        randomCode();
+        setCodeA(code);
+        let response = await fetch(config.urlRoot + 'createCodPaciente', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                terapeutaId: terapeutaId,
+                code: code
+            }),
+
+        });
+    }
+
+    function ListaPaciente({ nome, id }) {
+        if (nome != null) {
+            return (
+                <View>
+                    <Text>
+                        {nome}
+                        <TouchableOpacity style={css.login__button} onPress={() => onEdit(id, nome)} ><Text>E</Text></TouchableOpacity>
+                        <TouchableOpacity style={css.login__button} onPress={() => onDelete(id)} ><Text>D</Text></TouchableOpacity>
+                    </Text>
+                </View>
+            )
+        } else {
+            return (null);
         }
-        //Envio do formulário
-        async function sendForm()
-        {
-            randomCode();
-            setCodeA(code);
-            let response=await fetch(config.urlRoot+'createCodPaciente',{
-                method: 'POST',
-                headers: {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    terapeutaId: terapeutaId,
-                    code: code
-                }),
 
-            });
-        }
-
-        function ListaPaciente({nome,id}){
-            if(nome!=null){
-                return (
-                    <View>
-                        <Text>
-                            {nome}
-                            <TouchableOpacity style={css.login__button} onPress={() =>setModalVisible(true)} ><Text>E</Text></TouchableOpacity>
-                            <TouchableOpacity style={css.login__button} onPress={() => onDelete(id)} ><Text>D</Text></TouchableOpacity>
-                        </Text>
-                    </View>
-                )
-            }else{
-                return(null);
-            }
-            
-        }
-        return (
+    }
+    return (
         <View>
             <Text>Essa é a Home do Terapeuta</Text>
             <Text>Bem vindo {name}</Text>
@@ -186,27 +185,27 @@ export default function HomeTerapeuta() {
             </TouchableOpacity>
             <Text>{codeA}</Text>
             <View>
-        {(execucao==2)?
-            <Text>
-                <SafeAreaView style={css.container3}>
-                <FlatList
-                data={pacientes}
-                renderItem={({item}) => <ListaPaciente nome={item.name} id={item.id}/>}
-                keyExtractor={item => item.id.toString()}
-                />
-                </SafeAreaView>
-            </Text>
-            :
-            <View>
-            </View>
-        }
-         <View>
-                <Modal
-                    animationType="slide"
-                    visible={modalVisible}
-                >
-                     <View>
-                     <Text>{pacienteName}</Text>
+                {(execucao == 2) ?
+                    <Text>
+                        <SafeAreaView style={css.container3}>
+                            <FlatList
+                                data={pacientes}
+                                renderItem={({ item }) => <ListaPaciente nome={item.name} id={item.id} />}
+                                keyExtractor={item => item.id.toString()}
+                            />
+                        </SafeAreaView>
+                    </Text>
+                    :
+                    <View>
+                    </View>
+                }
+                <View>
+                    <Modal
+                        animationType="slide"
+                        visible={modalVisible}
+                    >
+                        <View>
+                            <Text>{paName}</Text>
                             <TextInput
                                 placeholder='Nome:'
                                 onChangeText={text => setpacienteName(text)}
@@ -219,18 +218,18 @@ export default function HomeTerapeuta() {
                                 placeholder='Telefone:'
                                 onChangeText={text => setpacienteTelefone(text)}
                             />
-                        <Pressable
-                            style={css.login__button}
-                            onPress={() => setModalVisible(!modalVisible)}
-                        >
-                            
-                        <Text >Hide Modal</Text>
-                        </Pressable>
+                            <Pressable
+                                style={css.login__button}
+                                onPress={() => setModalVisible(!modalVisible)}
+                            >
 
-                    </View>
-                            
-                </Modal>
-            </View>
+                                <Text >Hide Modal</Text>
+                            </Pressable>
+
+                        </View>
+
+                    </Modal>
+                </View>
             </View>
         </View>
 
