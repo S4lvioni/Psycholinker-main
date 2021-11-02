11351515cdec3dcb25439ace472d19059704ed35
GMDorneles <GMDorneles>
2021-11-02 17:00:16 -0400

    Revisão

--- views/Exercicios/Meditacao.js
+++ views/Exercicios/Meditacao.js
@@ -9,16 +9,16 @@ export default function Meditacao({ navigation }) {
     return (
         <View style={{backgroundColor:'#fff', justifyContent:'center',alignItems: 'center'}}>
             <Text style={{fontWeight:'bold', fontSize:18, marginTop:8}}>Meditação Mindfulness</Text>
-            <ScrollView style={{marginHorizontal:20}}>
+            <ScrollView style={{marginHorizontal:20,marginTop:10}}>
                 <View style={{backgroundColor:'#fff', justifyContent:'center',alignItems: 'center'}}>
-                    <Text style={{marginBottom:8, fontSize:16}}>1.Sente-se: Endireite as costas, permita-se relaxar um pouco e descanse em uma posição em que sua postura seja boa, sem ficar tenso ou rígido. </Text>
+                    <Text style={{marginBottom:8, fontSize:16, lineHeight:20}}>1.Sente-se: Endireite as costas, permita-se relaxar um pouco e descanse em uma posição em que sua postura seja boa, sem ficar tenso ou rígido. </Text>
                     <Image style={{ justifyContent:'center',alignItems: 'center', width:250, height:250}} source={require('./imagens/sentar.jpg')}/>
-                    <Text style={{marginTop:8, fontSize:16}}>2. Volte sua atenção para sua respiração: A simples observação da respiração acalma automaticamente o corpo e a mente e, como resultado, alonga e aprofunda a respiração. 
+                    <Text style={{marginTop:8, fontSize:16, lineHeight:20}}>2. Volte sua atenção para sua respiração: A simples observação da respiração acalma automaticamente o corpo e a mente e, como resultado, alonga e aprofunda a respiração. 
                     Concentre-se do início ao fim de cada inspiração e expiração. Concentrar-se no movimento da respiração através das narinas ou no movimento rítmico do abdômen ou do tórax o ajudará a manter o foco. </Text>
-                    <Text style={{ fontSize:16}}>3. Conte sua respiração: No final de cada expiração e inspiração, conte 1. Continue essa contagem até 10.Na maioria dos casos, você perderá o foco e diminuirá a contagem em algum lugar no início, geralmente entre 3 e 5, sendo gradual e consistentemente capaz de alcançar números mais altos à medida que pratica mais.</Text>
+                    <Text style={{ fontSize:16, lineHeight:20}}>3. Conte sua respiração: No final de cada expiração e inspiração, conte 1. Continue essa contagem até 10.Na maioria dos casos, você perderá o foco e diminuirá a contagem em algum lugar no início, geralmente entre 3 e 5, sendo gradual e consistentemente capaz de alcançar números mais altos à medida que pratica mais.</Text>
                     <Image style={{ justifyContent:'center',alignItems: 'center', width:250, height:250}} source={require('./imagens/Meditação-1.png')}/>
                    
-                    <Text style={{ fontSize:16}}>4. Observe + Refocalize sua atenção: Se você está perdendo sua atenção constantemente. Totalmente normal e nada com que se preocupar. Na verdade, no começo, é bom perceber isso. Se você está percebendo, significa que sua consciência está melhorando.
+                    <Text style={{ fontSize:16, lineHeight:20}}>4. Observe + Refocalize sua atenção: Se você está perdendo sua atenção constantemente. Totalmente normal e nada com que se preocupar. Na verdade, no começo, é bom perceber isso. Se você está percebendo, significa que sua consciência está melhorando.
                     Mas o que você faz a respeito?Observe a distração - mesmo que você simplesmente tenha percebido que ficou distraído em geral, mas não consegue sentir o que realmente o distraiu.
                     Comece rotulando-o de “distração”. Você gradualmente mudará para “pensamento”, “sentimento” ou “sensação” conforme sua consciência melhora. E, por fim, pensamento específico: “Estou ansioso com o futuro” ou emoção específica: “raiva”. Depois de reconhecer o que o distraiu, concentre-se novamente na respiração e continue contando. </Text>
                 </View>
